67b403dc50da81e9733910e5cca47f8e5eeb6193
NipunNegi <neginipun@gmail.com>
2021-03-12 00:46:34 -0500

    class added

---
+++ codeFirst/codeFirst/CodeFirstentity.cs
@@ -0,0 +1,14 @@
+﻿using codeFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace codeFirst
+{
+    public class CodeFirstentity:DbContext
+    {
+        public DbSet<student> student { get; set; }
+    }
+}
---
+++ codeFirst/codeFirst/Migrations/202103120359163_student table.cs
@@ -0,0 +1,28 @@
+﻿namespace codeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class studenttable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.students",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Address = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.students");
+        }
+    }
+}
